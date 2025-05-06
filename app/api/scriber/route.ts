12420a6235eb037b57a42335b9c032677550dffa
Dean van Niekerk <deanvniekerk@gmail.com>
2025-05-06 08:00:12 +0200

    add bottleneck

--- app/api/scriber/route.ts
+++ app/api/scriber/route.ts
@@ -1,8 +1,11 @@
 import type { AudioToTextResponse } from '@/app/types';
 import { NextResponse } from 'next/server';
 import { GoogleGenAI, createUserContent, createPartFromUri } from '@google/genai';
+import Bottleneck from 'bottleneck';
 
 const ai = new GoogleGenAI({ apiKey: process.env.GOOGLE_API_KEY });
+
+const limiter = new Bottleneck({ minTime: 60000 / 10 });
 
 export const POST = async (req: Request) => {
   const formData = await req.formData();
@@ -17,18 +20,6 @@ export const POST = async (req: Request) => {
 
     const filename = fileEntry.name;
 
-    // Create a transcription job
-    // const transcription = await groq.audio.transcriptions.create({
-    //   file: fileEntry,
-    //   // https://console.groq.com/docs/speech-text
-    //   model: 'whisper-large-v3-turbo', // better, bit more exp
-    //   // model: 'distil-whisper-large-v3-en',
-    //   prompt: 'Ophthalmologist doctors notes after a consult with patient. Expect medical terminology.',
-    //   response_format: 'json', // Optional
-    //   language: 'en', // Optional
-    //   temperature: 0.0, // Optional
-    // });
-
     const myfile = await ai.files.upload({
       file: fileEntry,
     });
@@ -36,13 +27,15 @@ export const POST = async (req: Request) => {
     if (!myfile.uri || !myfile.mimeType)
       return NextResponse.json({ error: 'No file provided or file is not valid.' }, { status: 400 });
 
-    const result = await ai.models.generateContent({
-      model: 'gemini-2.5-flash-preview-04-17',
-      contents: createUserContent([
-        createPartFromUri(myfile.uri, myfile.mimeType),
-        'Generate a transcript of the Ophthalmologist doctors notes after a consult with patient. Expect medical terminology.',
-      ]),
-    });
+    const result = await limiter.schedule(() =>
+      ai.models.generateContent({
+        model: 'gemini-2.5-flash-preview-04-17',
+        contents: createUserContent([
+          createPartFromUri(myfile.uri ?? '', myfile.mimeType ?? ''),
+          'Generate a transcript of the Ophthalmologist doctors notes after a consult with patient. Expect medical terminology.',
+        ]),
+      }),
+    );
 
     response.push({
       fileName: filename,
