877a530211894b75b3ce1523ac91fdb9ff01ea44
Dean van Niekerk <deanvniekerk@gmail.com>
2025-12-10 16:09:07 +0200

    upgrade libs

--- lib/ai.ts
+++ lib/ai.ts
@@ -1,2 +1,2 @@
-export const MODELS = ['gemini-2.5-flash-preview-05-20', 'gemini-2.0-flash'];
+export const MODELS = ['gemini-2.5-flash'];
 export const TEMPS = ['0', '0.1', '0.2', '0.3', '0.4', '0.5', '0.6', '0.7', '0.8', '0.9', '1'];
